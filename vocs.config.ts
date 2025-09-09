282826a333e9f9c6b62c8ab59aad19d0b776c7c6
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2025-09-09 17:58:11 +0200

    adding basic docs structure

--- vocs.config.ts
+++ vocs.config.ts
@@ -1,16 +1,66 @@
-import { defineConfig } from 'vocs'
+import { defineConfig } from "vocs";
 
 export default defineConfig({
-  title: 'Docs',
+  title: "Docs",
   sidebar: [
     {
-      text: 'Getting Started',
-      link: '/getting-started',
+      text: "What is Astroblock?",
+      link: "/what-is-astroblock",
     },
     {
-      text: 'Example',
-      link: '/example',
+      text: "Integration Options",
+      collapsed: false,
+      items: [
+        {
+          text: "Typescript SDK",
+          link: "/integrations/sdk",
+        },
+      ],
+    },
+    {
+      text: "Concepts",
+      collapsed: false,
+      items: [
+        {
+          text: "The Map",
+          link: "/concepts/the-map",
+        },
+      ],
+    },
+    {
+      text: "Use Cases",
+      collapsed: false,
+      items: [
+        {
+          text: "Stories",
+          link: "/use-cases/stories",
+        },
+      ],
+    },
+    {
+      text: "Reference",
+      collapsed: false,
+      items: [
+        {
+          text: "AstroblockMap",
+          link: "/reference/astroblockmap",
+        },
+      ],
     },
   ],
-  logoUrl: "https://app.astroblock.xyz/logo.png"
-})
+  socials: [
+    {
+      icon: "github",
+      link: "https://github.com/AstroblockStudio",
+    },
+    {
+      icon: "x",
+      link: "https://x.com/astroblockxyz",
+    },
+    {
+      icon: "warpcast",
+      link: "https://farcaster.xyz/~/channel/astroblock",
+    },
+  ],
+  logoUrl: "https://app.astroblock.xyz/logo.png",
+});
