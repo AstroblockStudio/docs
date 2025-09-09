62348cb3c314dc018b0416881a06f4a338be9894
Nezzar KEFIF <36443340+nezz0746@users.noreply.github.com>
2025-09-09 14:24:57 +0200

    initial commit

---
+++ vocs.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vocs'
+
+export default defineConfig({
+  title: 'Docs',
+  sidebar: [
+    {
+      text: 'Getting Started',
+      link: '/getting-started',
+    },
+    {
+      text: 'Example',
+      link: '/example',
+    },
+  ],
+  logoUrl: "https://app.astroblock.xyz/logo.png"
+})
